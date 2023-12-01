e04956211d51674f8d14761414182acdb5e186b2
Emiliano Machado <emiliano.a.machado@gmail.com>
2023-11-30 20:23:37 -0800

    [UPDATE - express server clean up & comments]

--- js/index.mjs
+++ js/index.mjs
@@ -1,11 +1,10 @@
 import express from "express";
 import mysql from 'mysql';
 
-
-const app = express();
+const app = express(); // define express server
 app.use(express.json());
 
-const db = mysql.createConnection({
+const db = mysql.createConnection({ // connect to MySQL DB
     user : 'root',
     password : 'gsv.092465',
     host : 'localhost',
@@ -14,13 +13,11 @@ const db = mysql.createConnection({
 
 db.connect();
 
-
-
-app.listen(3000, () =>
+app.listen(3000, () => // begin listen
   console.log('Example app listening on port 3000!'),
 );
 
-app.get('/data', (req,res) => {
+app.get('/data', (req,res) => { // return most recent DB SensorGrades table entry
     const result = db.query('SELECT * FROM SensorGrades ORDER BY idSensorGrades DESC LIMIT 1', function(error,results,fields){
         if (error) throw error;
         res.send(results)
